a7f208eb6d75c99258fe8a3e7dcc732cb05b8e8c
ioancristiansima <117097095+ioancristiansima@users.noreply.github.com>
2022-11-01 11:05:39 +0200

    Update pg1.php

--- pg1.php
+++ pg1.php
@@ -1,7 +1,7 @@
 
 <?php 
 if(isset($_POST['submit'])){
-    $to = "ioancristiansima@yahoo.com"; // this is your Email address
+    $to = "cristiansima1998@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
