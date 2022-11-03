a0f819275d13842865ea1c2dbee130bb79e53e5d
ioancristiansima <117097095+ioancristiansima@users.noreply.github.com>
2022-11-03 16:45:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<?php
+define('SITE_KEY', '6LeJTNciAAAAAEbVedqMXzg9i0LhqUg6HWxfMgZv');
+define('SECRET_KEY', '6LeJTNciAAAAAKe3VWOJ5eoGxSyVRMmKRLFxsnLc');
+
+if($_POST){
+    function getCaptcha($SecretKey){
+        $Response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".SECRET_KEY."&response={$SecretKey}");
+        $Return = json_decode($Response);
+        return $Return;
+    }
+    $Return = getCaptcha($_POST['g-recaptcha-response']);
+    //var_dump($Return);
+    if($Return->success == true && $Return->score > 0.5){
+        echo "Succes!";
+    }else{
+        echo "You are a Robot!!";
+    }
+}
+
+?>
+
+
+
+
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -89,6 +113,24 @@ $( "#myform" ).validate({
   }
 });
 </script>
+    
+    
+    
+    
+        <form action="/" method="POST">
+        <input type="text" name="name" /><br />
+        <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response" /><br >
+        <input type="submit" value="Submit" />
+    </form>
+    <script>
+    grecaptcha.ready(function() {
+    grecaptcha.execute('<?php echo SITE_KEY; ?>', {action: 'homepage'})
+    .then(function(token) {
+        //console.log(token);
+        document.getElementById('g-recaptcha-response').value=token;
+    });
+    });
+    </script>
   
 
   </body>
