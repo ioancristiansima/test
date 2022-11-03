b5c3b472e76f81c887547016819c9d7008f3bab5
ioancristiansima <117097095+ioancristiansima@users.noreply.github.com>
2022-11-03 17:00:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,3 @@
-<?php
-
-if(isset($_POST['submit']))
-{
-
-function CheckCaptcha($userResponse) {
-        $fields_string = '';
-        $fields = array(
-            'secret' => 6LeJTNciAAAAAKe3VWOJ5eoGxSyVRMmKRLFxsnLc
-            'response' => $userResponse
-        );
-        foreach($fields as $key=>$value)
-        $fields_string .= $key . '=' . $value . '&';
-        $fields_string = rtrim($fields_string, '&');
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
-        curl_setopt($ch, CURLOPT_POST, count($fields));
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
-
-        $res = curl_exec($ch);
-        curl_close($ch);
-
-        return json_decode($res, true);
-    }
-
-
-    // Call the function CheckCaptcha
-    $result = CheckCaptcha($_POST['g-recaptcha-response']);
-
-    if ($result['success']) {
-        //If the user has checked the Captcha box
-        echo "Captcha verified Successfully";
-	
-    } else {
-        // If the CAPTCHA box wasn't checked
-       echo '<script>alert("Error Message");</script>';
-    }
-}
-    ?>
-
-
 
 <html lang="en">
   <head>
@@ -136,9 +93,48 @@ $( "#myform" ).validate({
 });
 </script>
   
-  
+<?php
+
+if(isset($_POST['submit']))
+{
+
+function CheckCaptcha($userResponse) {
+        $fields_string = '';
+        $fields = array(
+            'secret' => 6LeJTNciAAAAAKe3VWOJ5eoGxSyVRMmKRLFxsnLc
+            'response' => $userResponse
+        );
+        foreach($fields as $key=>$value)
+        $fields_string .= $key . '=' . $value . '&';
+        $fields_string = rtrim($fields_string, '&');
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
+
+        $res = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($res, true);
+    }
+
+
+    // Call the function CheckCaptcha
+    $result = CheckCaptcha($_POST['g-recaptcha-response']);
+
+    if ($result['success']) {
+        //If the user has checked the Captcha box
+        echo "Captcha verified Successfully";
+	
+    } else {
+        // If the CAPTCHA box wasn't checked
+       echo '<script>alert("Error Message");</script>';
+    }
+}
+    ?>
 
   </body>
 </html>
-
 
