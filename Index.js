2753287ac59702cd53d7a30c43db2ababce50412
ioancristiansima <117097095+ioancristiansima@users.noreply.github.com>
2022-11-03 15:22:46 +0200

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,22 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_nlq3h1n";
+  const templateID = "template_1pf7or5";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
