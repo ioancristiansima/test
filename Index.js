a3cdda591408b739d1a03503d266d8cbd6bc2528
ioancristiansima <117097095+ioancristiansima@users.noreply.github.com>
2022-11-07 10:29:34 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -11,16 +11,24 @@ function sendMail() {
 
 
   
-if(grecaptcha.getResponse().length ==0)
+if(grecaptcha && grecaptcha.getResponse().length > 0)
 {
- 
-          alert("nu e selectat")
-  
-      }
+  if(document.getElementById("name").value&&document.getElementById("email").value&&document.getElementById("message").value){
 
+  
+    emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
     }else{
       
-      alert("se trimite")
+      alert("Your message don't sent !!")
     }
      
      
@@ -33,5 +41,3 @@ else
 }
 
 }
-
-
